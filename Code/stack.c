ef3341d5cdd54406090d3242c0ec993ad864eebf
locw <locw@pc3-003-l.cs.st-andrews.ac.uk>
2018-02-23 10:44:16 +0000

    Shortened code to remove some print debug statements.

--- Code/stack.c
+++ Code/stack.c
@@ -5,16 +5,13 @@ void print_frame(); //TODO split into function for printing
 /* Print three stack frames, of the three callers directly above.
  */
 void print_stack() {
+    //Stores current and previous base pointer.
     long rbp, rbp1;
 
     //Moves value in register rbp into variable rbp
     asm("movq %%rbp, %0;" : "=r"(rbp));
-    //Prints value of rbp.
-    printf("RBP %lx\n", rbp);
     //stores value rbp points to (previous base pointer) in rbp1
     rbp1 = *((long*) rbp);
-    //Prints previous base pointer.
-    printf("RBP1 %lx\n\n", rbp1);
 
     //Calls method to print 3 stack frames
     print_frame(rbp, rbp1, 3);
